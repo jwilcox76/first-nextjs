81d0407f575ba046115213afa411ec2a4c699ed0
Joshua Wilcox <jwilcoxsoccer76@gmail.com>
2024-10-31 14:39:11 -0600

    added counter

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,8 +15,14 @@ export default function Home() {
       </Head>
 
       <h1 className={styles.title}>
-          Joshua Wilcox welcomes <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        Joshua Wilcox welcomes <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <div>
+        <h2>Counter: {count}</h2>
+        <button onClick={increment} >Increase</button>
+        <button onClick={decrement} >Decrease</button>
+      </div>
     </div>
   );
 }
