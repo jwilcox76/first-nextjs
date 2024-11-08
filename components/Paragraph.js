5f8751f43164270d991e781bb03c9d307b8afd4b
Joshua Wilcox <jwilcoxsoccer76@gmail.com>
2024-11-08 13:21:26 -0700

    paragraph change

--- components/Paragraph.js
+++ components/Paragraph.js
@@ -2,8 +2,9 @@ function Paragraph(){
 
 return(
     <p><u><a href="https://github.com/jwilcox76/first-nextjs">Github link</a></u>
-    <br/> For the final project my team is modifying the htmx todo app to include user authentication so that a user can add other users and see their todos
-    <br/> We will showcase this by demoing the user's ability to search and find other users and decide if they want their own todos to be public or private</p>
+    For the final project my team is creating a "twitter-like" social media user experience<br/>
+    TO make this project we will be using htmx. 
+    <br/>We will be showcasing our project by adding other users to view their "tweets"<br/> </p>
 
     );
 }
